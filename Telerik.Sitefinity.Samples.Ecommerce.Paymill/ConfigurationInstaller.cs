786bb222a2697b3f9c090bb44f0398a6625abf49
Ivan Petrov <ivan.a.petrov@telerik.com>
2013-07-31 11:53:52 +0300

    Initial commit

---
+++ Telerik.Sitefinity.Samples.Ecommerce.Paymill/ConfigurationInstaller.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Abstractions.VirtualPath.Configuration;
+using Telerik.Sitefinity.Configuration;
+using Telerik.Sitefinity.Data;
+using Telerik.Sitefinity.Modules.Ecommerce.Orders.Configuration;
+
+namespace Telerik.Sitefinity.Samples.Ecommerce.Paymill
+{
+    public class ConfigurationInstaller
+    {
+        public static void PreApplicationStart()
+        {
+            Bootstrapper.Initialized += (new EventHandler<ExecutedEventArgs>(ConfigurationInstaller.Bootstrapper_Initialized));
+        }
+
+        private static void Bootstrapper_Initialized(object sender, ExecutedEventArgs e)
+        {
+            if (e.CommandName != "RegisterRoutes" || !Bootstrapper.IsDataInitialized)
+            {
+                return;
+            }
+
+            InstallPaymillVirtualPath();
+            InstallPaymillConfiguration();
+        }
+  
+        private static void InstallPaymillVirtualPath()
+        {
+            var virtualPathConfig = Config.Get<VirtualPathSettingsConfig>();
+            var key = PaymillProvider.PAYMILL_VIRTUAL_PATH + "*";
+            if (!virtualPathConfig.VirtualPaths.ContainsKey(key))
+            {
+                var configManager = ConfigManager.GetManager();
+                virtualPathConfig = configManager.GetSection<VirtualPathSettingsConfig>();
+                virtualPathConfig.VirtualPaths.Add(new VirtualPathElement(virtualPathConfig.VirtualPaths)
+                {
+                    VirtualPath = key,
+                    ResolverName = "EmbeddedResourceResolver",
+                    ResourceLocation = "Telerik.Sitefinity.Samples.Ecommerce.Paymill"
+                });
+                configManager.SaveSection(virtualPathConfig);
+            }
+        }
+
+        public static void InstallPaymillConfiguration()
+        {
+            var config = Config.Get<PaymentProcessorConfig>();
+            var key = PaymillProvider.GATEWAY_NAME;
+            if (!config.PaymentProcessorProviders.ContainsKey(key))
+            {
+                var configManager = ConfigManager.GetManager();
+                config = configManager.GetSection<PaymentProcessorConfig>();
+                config.PaymentProcessorProviders.Add(new PaymentProcessorProviderSettings(config.PaymentProcessorProviders)
+                {
+                    Id = PaymillProvider.PaymillProcessorId,
+                    Name = key,
+                    Title = "Paymill",
+                    IsActive = true,
+                    EnableLogging = false,
+                    Description = "Paymill Payment Proiveder",
+                    SectionName = "PaymillSettingsSection",
+                    SectionCssClass = "sf_paymentSettingsField sf_paymillPaymentSettingsField sfSectionInSection sfDisplayNone",
+                    SettingsType = typeof(PaymillSettings),
+                    ViewProviderType = typeof(PaymillSettingsField),
+                    ProviderType = typeof(PaymillProvider)
+                });
+                configManager.SaveSection(config);
+            }
+        }
+    }
+}
---
+++ Telerik.Sitefinity.Samples.Ecommerce.Paymill/Settings/PaymillSettings.cs
@@ -0,0 +1,98 @@
+﻿using Telerik.Sitefinity.Ecommerce.Payment.Model;
+
+namespace Telerik.Sitefinity.Samples.Ecommerce.Paymill
+{
+    public class PaymillSettings : IPaymentSettings
+    {
+        /// <summary>
+        /// Returns test or live URL depending on value of TestMode property
+        /// </summary>
+        public string GatewayUrl
+        {
+            get
+            {
+                return TestMode ? TestGatewayUrl : LiveGatewayUrl;
+            }
+        }
+
+        public string PrivateApiKey
+        {
+            get
+            {
+                return TestMode ? TestPrivateApiKey : LivePrivateApiKey;
+            }
+        }
+
+        public string PublicApiKey
+        {
+            get
+            {
+                return TestMode ? TestPublicApiKey : LivePublicApiKey;
+            }
+        }
+
+        /// <summary>
+        /// The Gateway URL for Test mode
+        /// </summary>
+        public string TestGatewayUrl { get; set; }
+
+        /// <summary>
+        /// The Private Test API Key provided by the Payment Processor ( Paymill ). 
+        /// </summary>
+        public string TestPrivateApiKey { get; set; }
+
+        /// <summary>
+        /// The Public Test API Key provided by the Payment Processor ( Paymill ). 
+        /// </summary>
+        public string TestPublicApiKey { get; set; }
+
+        /// <summary>
+        /// The Gateway URL for Live mode
+        /// </summary>
+        public string LiveGatewayUrl { get; set; }
+
+        /// <summary>
+        /// The Private Live API Key provided by the Payment Processor ( Paymill ). 
+        /// </summary>
+        public string LivePrivateApiKey { get; set; }
+
+        /// <summary>
+        /// The Public Live API Key provided by the Payment Processor ( Paymill ). 
+        /// </summary>
+        public string LivePublicApiKey { get; set; }
+
+        /// <summary>
+        /// Returns test or live URL depending on value of TestMode property
+        /// </summary>
+        public bool TestMode
+        {
+            get
+            {
+                return ProcessingMode.ToLower() == "live" ? false : true;
+            }
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Processing Mode ("test", "live")
+        /// </summary>
+        public string ProcessingMode { get; set; }
+
+        /// <summary>
+        /// Timeout (in milliseconds) for post to payment processor
+        /// </summary>
+        public double Timeout { get; set; }
+
+        /// <summary>
+        /// Array of strings that are names of credit cards (lower case) processed by the payment processor
+        /// </summary>
+        public string[] ProcessorCreditCards { get; set; }
+
+        /// <summary>
+        /// Payment type (sale, auth, capture, auth and capture, etc)
+        /// </summary>
+        public string PaymentType { get; set; }
+    }
+}
